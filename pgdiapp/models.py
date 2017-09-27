3ceba948d54624cb8e3bf2caf4e03436151742d9
javige <javier.gestevez@iesfernandoaguilar.es>
2017-09-27 02:56:27 +0200

    Update models.py

--- pgdiapp/models.py
+++ pgdiapp/models.py
@@ -1,4 +1,7 @@
+#-*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator, MaxLengthValidator, MinLengthValidator
@@ -8,7 +11,7 @@ from localflavor.es.forms import ESIdentityCardNumberField
 
 class Taller(models.Model):
 	cod = models.CharField(max_length=5, unique=True)
-	numero = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(4)])
+	numero = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(3)])
 	info = models.TextField(default=None, blank=True, null=True)
 	def __str__(self):
 		return str(self.cod)
@@ -19,6 +22,7 @@ class Taller(models.Model):
 class Grado(models.Model):
 	cod = models.CharField(max_length=5, unique=True)
 	nombre = models.CharField(max_length=200, unique=True)
+	gid = models.CharField(max_length=5, unique=True)
 	nocturno = models.BooleanField(default=False)
 	def __str__(self):
 		return str(self.cod)
@@ -30,11 +34,13 @@ class Clase(models.Model):
 	grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
 	taller = models.ForeignKey(Taller, on_delete=models.CASCADE)
 	curso = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(2)])
+	grupo = models.CharField(max_length=1, default='A')
+	tarde = models.BooleanField(default=False)
 	def __str__(self):
-		return str(self.grado) + " - " + str(self.taller)
+		return str(self.curso) + str(self.grupo) + ' ' + str(self.grado)
 	class Meta:
-		ordering = ["grado","taller"]
-		unique_together = ('grado', 'taller')
+		ordering = ["grado","curso","grupo","taller"]
+		#unique_together = ('grado', 'taller')
 		verbose_name_plural = "Clases"
 
 class Perfil(models.Model):
@@ -58,36 +64,43 @@ class Perfil(models.Model):
 	comunidad = models.CharField(max_length=50, null=True)
 	pais = models.CharField(max_length=50, null=True)
 	validado = models.BooleanField(default=False)
+
+	estudios = models.CharField(max_length=50, null=True, blank=False)
+	trabajo = models.CharField(max_length=50, null=True, blank=True)
+
 	def __str__(self):
 		return str(self.user.username)
 	class Meta:
 		ordering = ["user"]
 		verbose_name_plural = "Perfiles"
 
+@python_2_unicode_compatible
 class Pregunta(models.Model):
 	texto = models.CharField(max_length=300, null=False)
 	def __str__(self):
-		return str(self.texto)
+		return unicode(self.texto)
 	class Meta:
-		ordering = ["texto"]
+		#ordering = ["texto"]
 		verbose_name_plural = "Preguntas"
 
+@python_2_unicode_compatible
 class Cuestionario(models.Model):
 	grado = models.ForeignKey(Grado, on_delete=models.CASCADE)
 	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
 	def __str__(self):
-		return str(self.grado)+": "+str(self.pregunta)
+		return unicode(self.grado)+": "+unicode(self.pregunta)
 	class Meta:
 		ordering = ["grado","pregunta"]
 		unique_together = ('grado','pregunta')
 		verbose_name_plural = "Cuestionarios"
 
+@python_2_unicode_compatible
 class Respuesta(models.Model):
 	alumno = models.ForeignKey(Perfil, on_delete=models.CASCADE)
 	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
-	texto = models.CharField(max_length=300, null=False)
+	texto = models.TextField(max_length=1000, blank=True, null=True)
 	def __str__(self):
-		return str(self.alumno)+" - "+str(self.pregunta)+": "+str(self.texto)
+		return unicode(self.alumno)+" - "+unicode(self.pregunta)+": "+unicode(self.texto)
 	class Meta:
 		ordering = ["alumno","pregunta"]
 		unique_together = ('alumno','pregunta')
